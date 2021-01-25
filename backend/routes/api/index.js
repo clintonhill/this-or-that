ef213c0140a10930691753c6fd6995f94d43633b
Clint Hill <clint.hill07@gmail.com>
2021-01-25 11:36:28 -0500

    Init project

---
+++ backend/routes/api/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
---
+++ backend/routes/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+
+  router.use(express.static(path.resolve("../frontend/build")));
+
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+module.exports = router;
