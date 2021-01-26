7c5bf4b2c804b50838f4b0398c45aace3c6e4fdd
Clint Hill <clint.hill07@gmail.com>
2021-01-26 17:29:58 -0500

    Work on CSS on graph page

--- frontend/src/components/QuestionsPage/QuestionsPage.js
+++ frontend/src/components/QuestionsPage/QuestionsPage.js
@@ -12,39 +12,39 @@ function QuestionsPage({questionDetails}) {
   const {question, answer, chartData} = questionDetails;
 
   return (
-    <>
-    <div className='question-box'>
-      <div className='main-header'>
-        Question:
+    <div className='container'>
+      <div className='question-box'>
+        <div className='main-header'>
+          Question:
+        </div>
+        <div className='question-header'>
+          {question.title}
+        </div>
+        <div className='question-detail'>
+          {question.body}
+        </div>
       </div>
-      <div className='question-header'>
-        {question.title}
+      <div className='answer-box'>
+        <div className='main-header'>
+          Answer:
+        </div>
+        <div className='answer-header'>
+          You chose {answer.chose}
+        </div>
       </div>
-      <div className='question-detail'>
-        {question.body}
+      <div className='chart-container'>
+        <Pie
+          className='pie-chart'
+          data={{
+            labels: chartData.labels,
+            datasets: [{
+              data: chartData.votes,
+              backgroundColor: chartData.backgroundColor
+            }]
+          }}
+        />
       </div>
     </div>
-    <div className='answer-box'>
-      <div className='main-header'>
-        Answer:
-      </div>
-      <div className='answer-header'>
-        You chose {answer.chose}
-      </div>
-    </div>
-    <div className='chart-container'>
-      <Pie
-        className='pie-chart'
-        data={{
-          labels: chartData.labels,
-          datasets: [{
-            data: chartData.votes,
-            backgroundColor: chartData.backgroundColor
-          }]
-        }}
-      />
-    </div>
-    </>
   )
 }
 
---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Sidebar.css';
+
+function Sidebar(){
+  return (
+    <div className='sidenav-container'>
+      <ul>
+        <li>
+          <NavLink exact to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink exact to="#">Profile</NavLink>
+        </li>
+        <li>
+          <NavLink exact to="#">Ask a Question</NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/questions">Browse Questions</NavLink>
+        </li>
+        <li>
+          <NavLink exact to="#">Answer Random Question</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
