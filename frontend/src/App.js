e454e25343d13ee2ca1ce9c197029d5325a3bfed
Clint Hill <clint.hill07@gmail.com>
2021-01-26 13:18:58 -0500

    Added graph package and render dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,19 @@ const questionMock = {
   },
   answer: {
     chose: 'Rorschach'
+  },
+  chartData: {
+    labels: ['Rorschach', 'Spiderman', 'Wonder Woman', 'Ant-Man', 'Dr Strange', 'Thor', 'Batman'],
+    votes: [10, 5, 6, 2, 3, 8, 8 ],
+    backgroundColor: [
+    "rgb(255, 99, 132)",
+    "rgb(255, 159, 64)",
+    "rgb(255, 205, 86)",
+    "rgb(75, 192, 192)",
+    "rgb(54, 162, 235)",
+    "rgb(153, 102, 255)",
+    "rgb(155, 49, 146)",
+  ]
   }
 }
 
--- frontend/src/components/QuestionsPage/QuestionsPage.js
+++ frontend/src/components/QuestionsPage/QuestionsPage.js
@@ -3,11 +3,14 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+import { Pie, Chart } from 'react-chartjs-2';
+
 import './QuestionsPage.css';
 
 function QuestionsPage({questionDetails}) {
 
-  const {question, answer} = questionDetails;
+  const {question, answer, chartData} = questionDetails;
+
   return (
     <>
     <div className='question-box'>
@@ -28,6 +31,18 @@ function QuestionsPage({questionDetails}) {
       <div className='answer-header'>
         You chose {answer.chose}
       </div>
+    </div>
+    <div className='chart-container'>
+      <Pie
+        className='pie-chart'
+        data={{
+          labels: chartData.labels,
+          datasets: [{
+            data: chartData.votes,
+            backgroundColor: chartData.backgroundColor
+          }]
+        }}
+      />
     </div>
     </>
   )
